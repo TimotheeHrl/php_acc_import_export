9164af42f4fce8c9cb622a9f92194f073026067a
timotheehrl <tharleaux@gmail.com>
2022-11-16 11:58:16 +0100

    a

--- src/Controller/AccidentController.php
+++ src/Controller/AccidentController.php
@@ -11,12 +11,18 @@ use App\Repository\LieuxRepository;
 use App\Repository\UsagersRepository;
 use App\Repository\VehiculeRepository;
 use App\Entity\Caracteristique;
-
+use Symfony\Component\HttpFoundation\HeaderUtils;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class AccidentController extends AbstractController
 {
     /**
-     * @Route("/accident", name="app_usager")
+     * @Route("/accident", name="Accident_File_Json")
      */
     public function getAccidentJsonFile(
         CaracteristiqueRepository $caracteristiqueRepository,
@@ -24,20 +30,65 @@ class AccidentController extends AbstractController
         UsagersRepository $usagersRepository,
         VehiculeRepository $vehiculeRepository,
         string $num_Acc
-    ): JsonResponse {
-        $Accident = [];
+    ): Response {
+        $accident = [];
 
         $caracteristiques = $caracteristiqueRepository->findByNum_Acc($num_Acc);
         $lieux = $lieuxRepository->findByNum_Acc($num_Acc);
         $usagers = $usagersRepository->findByNum_Acc($num_Acc);
         $vehicules = $vehiculeRepository->findByNum_Acc($num_Acc);
 
-        $Accident['caracteristiques'] = $caracteristiques;
-        $Accident['lieux'] = $lieux;
-        $Accident['usagers'] = $usagers;
-        $Accident['vehicules'] = $vehicules;
+        $accident['caracteristiques'] = $caracteristiques;
+        $accident['lieux'] = $lieux;
+        $accident['usagers'] = $usagers;
+        $accident['vehicules'] = $vehicules;
+        $fileContent = $this->json($accident); // the generated file content
+        $response = new Response($fileContent);
+
+        $disposition = HeaderUtils::makeDisposition(
+            HeaderUtils::DISPOSITION_ATTACHMENT,
+            'foo.json'
+        );
+        $response->headers->set('Content-Disposition', $disposition);
+        return $response;
+    }
+
+    /**
+     * @Route("/accident/{num_Acc}", name="Accident_File_Csv")
+     */
+    public function getAccidentCsvFile(
+        CaracteristiqueRepository $caracteristiqueRepository,
+        LieuxRepository $lieuxRepository,
+        UsagersRepository $usagersRepository,
+        VehiculeRepository $vehiculeRepository,
+        string $num_Acc
+    ): Response {
+        $accident = [];
+
+        $caracteristiques = $caracteristiqueRepository->findByNum_Acc($num_Acc);
+        $lieux = $lieuxRepository->findByNum_Acc($num_Acc);
+        $usagers = $usagersRepository->findByNum_Acc($num_Acc);
+        $vehicules = $vehiculeRepository->findByNum_Acc($num_Acc);
+
+        $accident['caracteristiques'] = $caracteristiques;
+        $accident['lieux'] = $lieux;
+        $accident['usagers'] = $usagers;
+        $accident['vehicules'] = $vehicules;
+
+        $encoders = [new CsvEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $fileContent = $serializer->serialize($accident, 'csv'); // the generated file content
 
 
-        return $this->json($Accident);
+        $response = new Response($fileContent);
+
+        $disposition = HeaderUtils::makeDisposition(
+            HeaderUtils::DISPOSITION_ATTACHMENT,
+            'foo.csv'
+        );
+        $response->headers->set('Content-Disposition', $disposition);
+        return $response;
     }
 }
