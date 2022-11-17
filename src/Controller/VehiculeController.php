588b8455c81d4e6dd9b8b78b90e45dd988d70c1e
timotheehrl <tharleaux@gmail.com>
2022-11-17 12:14:15 +0100

    a

--- src/Controller/VehiculeController.php
+++ src/Controller/VehiculeController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controller;
 
+use App\Form\VehiculeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\VehiculeRepository;
-
+use Symfony\Component\String\Slugger\SluggerInterface;
+use App\Entity\Vehicule;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class VehiculeController extends AbstractController
 {
@@ -37,5 +43,40 @@ class VehiculeController extends AbstractController
     {
         $data = $vehiculeRepository->findByNum_Acc($num_Acc);
         return $this->json($data);
+    }
+
+
+    // method post that parse csv file and insert data into database&
+    /**
+     * @Route("/api/vehicule/{num_Acc}", name="searchByNum_Acc_vehicule", methods={"GET","HEAD"})
+     */
+    public function new(Request $request, SluggerInterface $slugger): Response
+    {
+        if (!$request->files->get('veh')) {
+            return new Response('No file uploaded', 400);
+        } else {
+            /** @var UploadedFile $uploadedFile */
+            $uploadedFile = $request->files->get('veh');
+            $destination = $this->getParameter('kernel.project_dir') . '/public/uploads';
+            $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+            // this is needed to safely include the file name as part of the URL
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename . '-' . uniqid() . '.csv';
+            $uploadedFile->move($destination, $newFilename);
+            $fileData = fopen($destination . '/' . $newFilename, 'r');
+            // csv parser in php
+            $i = 0;
+            $aa = [];
+            while (($column = fgetcsv($fileData, 10000, ";")) !== FALSE) {
+                if ($i > 0) {
+
+                    $aa = $column[0];
+                }
+                $i++;
+            }
+
+            dd($aa);
+            return new Response("aa", 200);
+        }
     }
 }
